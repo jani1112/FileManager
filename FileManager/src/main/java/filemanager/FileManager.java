4d6cca3bd00fd0519511ea731bbfdb12cb11c5d0
027950206 <027950206@host.docker.internal>
2022-04-18 17:46:15 -0700

    update commit v3

--- FileManager/src/main/java/filemanager/FileManager.java
+++ FileManager/src/main/java/filemanager/FileManager.java
@@ -2,13 +2,16 @@ package filemanager;
 
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.util.Iterator;
+import java.util.Vector;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDesktopPane;
+import javax.swing.JInternalFrame;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,11 +19,15 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
 
 /**
  *
@@ -28,51 +35,21 @@ import javax.swing.tree.DefaultTreeModel;
  */
 public class FileManager {
     
-    private final JFrame frame;
-    private final JPanel leftpanel,rightpanel,toppanel;
-    private final JSplitPane splitpane;
     private JMenuBar jmenubar;
     private JMenu menu1,menu2,menu3,menu4;  
     private JMenuItem menuitem11, menuitem12, menuitem13, menuitem14,menuitem15,menuitem21, menuitem22,menuitem31, menuitem32,menuitem41, menuitem42;
     private static JDialog jdbox;
     private JButton btn;
-    private final JComboBox cb;
-    private final JToolBar toolbar;
-    //private final JTree directorytree;
+
+  
     
-    private FileManager(){
-      /**
-     *Method to generate the directory structure on left panel
-     */
-//    directorytree = new JTree();
-//    File fileRoot = new File("C:/");
-    
-    frame = new JFrame();
-    AddMenu();
-    toppanel = new JPanel();
-    File[] drives = File.listRoots();
-    toolbar = new JToolBar();
-    cb = new JComboBox(drives);
-    cb.setBounds(50, 50,90,20); 
-    toppanel.add(cb);
-    toppanel.add(new JButton("Simple"));
-    toppanel.add(new JButton("Detail"));
-    toolbar.add(toppanel);
-    leftpanel = new JPanel();
-    //leftpanel.add(directorytree);
-    rightpanel = new JPanel();
-    splitpane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftpanel, rightpanel);
-    splitpane.setResizeWeight(0.3);
-    frame.add(toppanel,BorderLayout.NORTH);
-    frame.add(splitpane);
-    frame.setSize(800,600);
-    frame.setTitle("CECS 544 File Manager");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
-    }
+    public void FileManager() {
+        
+        
+         }
     
     
-    public final void AddMenu(){
+    public final void AddMenu(JFrame mainframe){
     
     jmenubar  = new JMenuBar(); 
     menu1 = new JMenu("File");
@@ -112,13 +89,13 @@ public class FileManager {
           
           //property for menuitems
           menuitem15.addActionListener((var e3)->{
-              frame.dispose();
+              mainframe.dispose();
     });
           
           
           menuitem42.addActionListener((ActionEvent e) -> {
           Box box = new Box(BoxLayout.Y_AXIS);
-          jdbox = new JDialog(frame,"About Product",true);
+          jdbox = new JDialog(mainframe,"About Product",true);
           btn = new JButton("Ok");
           //btn.setAlignmentX(btn.CENTER_ALIGNMENT);
           btn.addActionListener((ActionEvent e1) -> {
@@ -136,13 +113,153 @@ public class FileManager {
     jmenubar.add(menu2);
     jmenubar.add(menu3);
     jmenubar.add(menu4);
-    frame.setJMenuBar(jmenubar); 
+    mainframe.setJMenuBar(jmenubar); 
     
         }
-       
-  
+      
     public static void main(String[] args) { 
+    
+        JFrame frame = new JFrame();
+        JPanel toppanel = new JPanel();
+        File[] drives = File.listRoots();
         
-        FileManager fileManager = new FileManager();
+        JToolBar toolbar = new JToolBar();
+        JComboBox cb = new JComboBox(drives);
+        cb.setBounds(50, 50,90,20);   
+        toppanel.add(cb);
+        toppanel.add(new JButton("Simple"));
+        toppanel.add(new JButton("Details"));
+        toolbar.add(toppanel);
+       
+        
+        FileManager f  = new FileManager();
+        f.AddMenu(frame);
+        
+        
+        frame.setSize(1000,650);
+        frame.setTitle("CECS 544 File Manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        JDesktopPane desk = new JDesktopPane();
+        //desk.setLayout(new GridBagLayout());
+//        JInternalFrame frame1 = new JInternalFrame("Internal Frame",false,true,true,true);
+//        frame1.setSize(700, 500);
+//        frame1.setVisible(true);
+//        desk.add(frame1);
+
+        
+        JPanel leftpanel = new JPanel();
+
+        JPanel rightpanel = new JPanel();
+        
+        //Sroll Pane
+        JScrollPane leftscrollpane = new JScrollPane(leftpanel);
+        leftscrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS); 
+        JScrollPane rightscrollpane = new JScrollPane(rightpanel);
+        rightscrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+   
+        //Get Directory Data
+        String GetCurrDir = cb.getSelectedItem().toString();
+        FileSystemModel fileSystemModel = new FileSystemModel(new File("D:\\"));
+        JTree fileTree = new JTree(fileSystemModel);
+        fileTree.setEditable(true);
+        leftpanel.add(fileTree);
+        
+        JSplitPane splitpane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,true, leftscrollpane, rightscrollpane);
+
+        splitpane.setResizeWeight(0.3);
+        frame.add(toppanel,BorderLayout.NORTH);
+        frame.getContentPane().add(splitpane);
+       // frame.add(desk);
+     
+        frame.setVisible(true);
+       
+    }   
+          
     }
+
+class FileSystemModel implements TreeModel {
+  private File root;
+ 
+  private Vector listeners = new Vector();
+ 
+  public FileSystemModel(File rootDirectory) {
+    root = rootDirectory;
+  }
+ 
+  public Object getRoot() {
+    return root;
+  }
+ 
+  public Object getChild(Object parent, int index) {
+    File directory = (File) parent;
+    String[] children = directory.list();
+    return new TreeFile(directory, children[index]);
+  }
+ 
+  public int getChildCount(Object parent) {
+    File file = (File) parent;
+    if (file.isDirectory()) {
+      String[] fileList = file.list();
+      if (fileList != null)
+        return file.list().length;
+    }
+    return 0;
+  }
+ 
+  public boolean isLeaf(Object node) {
+    File file = (File) node;
+    return file.isFile();
+  }
+ 
+  public int getIndexOfChild(Object parent, Object child) {
+    File directory = (File) parent;
+    File file = (File) child;
+    String[] children = directory.list();
+    for (int i = 0; i < children.length; i++) {
+      if (file.getName().equals(children[i])) {
+        return i;
+      }
+    }
+    return -1;
+ 
+  }
+ 
+  public void valueForPathChanged(TreePath path, Object value) {
+    File oldFile = (File) path.getLastPathComponent();
+    String fileParentPath = oldFile.getParent();
+    String newFileName = (String) value;
+    File targetFile = new File(fileParentPath, newFileName);
+    oldFile.renameTo(targetFile);
+    File parent = new File(fileParentPath);
+    int[] changedChildrenIndices = { getIndexOfChild(parent, targetFile) };
+    Object[] changedChildren = { targetFile };
+    fireTreeNodesChanged(path.getParentPath(), changedChildrenIndices, changedChildren);
+ 
+  }
+ 
+  private void fireTreeNodesChanged(TreePath parentPath, int[] indices, Object[] children) {
+    TreeModelEvent event = new TreeModelEvent(this, parentPath, indices, children);
+    Iterator iterator = listeners.iterator();
+    TreeModelListener listener = null;
+    while (iterator.hasNext()) {
+      listener = (TreeModelListener) iterator.next();
+      listener.treeNodesChanged(event);
+    }
+  }
+ 
+  public void addTreeModelListener(TreeModelListener listener) {
+    listeners.add(listener);
+  }
+ 
+  public void removeTreeModelListener(TreeModelListener listener) {
+    listeners.remove(listener);
+  }
+ 
+  private class TreeFile extends File {
+    public TreeFile(File parent, String child) {
+      super(parent, child);
+    }
+  }
 }
